e08d74d0bf8ccfd04a49f0fcec48e2756bfb89d9
Constantine <77935386+velteren@users.noreply.github.com>
2023-12-18 13:27:53 +0300

    feat: add solution

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -24,6 +24,5 @@ public class RepeatIsBad {
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
-        System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
     }
 }
